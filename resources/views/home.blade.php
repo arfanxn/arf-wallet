eea6f310d782f0cd09123ab09f14a21f8165f77c
mysql73 <arfan2173@gmail.com>
2021-11-06 01:22:18 +0700

    creating and configuring component app.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout title="Hello">
+    <div class="p-5 bg-danger">Hello World</div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
+    {{-- Fonts --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    {!! $styles ?? '' !!}
+    <title>{{ $title . ' | ' . config('app.name', 'Laravel') }}</title>
+
+</head>
+
+<body>
+
+    <main class="container-md">
+        {{ $slot }}
+    </main>
+
+    {{-- pooperJs and jQuery --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- pooperJs and jQuery --}}
+    {!! $scripts ?? '' !!}
+</body>
+
+</html>
