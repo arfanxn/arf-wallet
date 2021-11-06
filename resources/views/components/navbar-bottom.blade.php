0514371e77a605631e417eb9fdcb1a18f1e5a8a4
mysql73 <arfan2173@gmail.com>
2021-11-07 02:08:39 +0700

    fixing links in navbar-bottom

--- resources/views/components/navbar-bottom.blade.php
+++ resources/views/components/navbar-bottom.blade.php
@@ -1,16 +1,16 @@
 <nav class="fixed-bottom bg-light offset-md-4 col-12 col-md-4 border-top border-dark pt-1  ">
     <div class="row text-center">
-        <div class="col-4">
+        <div class="col-4 ">
             <x-icon.history></x-icon.history>
-            <p class="p-0 m-0">Riwayat</p>
+            <a href="" class="text-decoration-none text-dark d-block">Riwayat</a>
         </div>
-        <div class="col-4">
+        <div class="col-4 ">
             <x-icon.home></x-icon.home>
-            <p class="p-0 m-0">Beranda</p>
+            <a href="" class="text-decoration-none text-dark d-block">Beranda</a>
         </div>
-        <div class="col-4">
+        <div class="col-4 ">
             <x-icon.user-profile></x-icon.user-profile>
-            <p class="p-0 m-0">Profil</p>
+            <a href="" class="text-decoration-none text-dark d-block">Profil</a>
         </div>
     </div>
 </nav>
