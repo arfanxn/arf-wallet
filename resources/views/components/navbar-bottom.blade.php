2d69bb43c3ffef739a49c71d1dbbdc4d393f6680
mysql73 <arfan2173@gmail.com>
2021-11-07 02:20:35 +0700

    download icon for money transactions

--- resources/views/components/navbar-bottom.blade.php
+++ resources/views/components/navbar-bottom.blade.php
@@ -1,16 +1,19 @@
 <nav class="fixed-bottom bg-light offset-md-4 col-12 col-md-4 border-top border-dark pt-1  ">
     <div class="row text-center">
         <div class="col-4 ">
-            <x-icon.history></x-icon.history>
-            <a href="" class="text-decoration-none text-dark d-block">Riwayat</a>
+            <a href="" class="text-decoration-none text-dark ">
+                <x-icon.history></x-icon.history><br>Riwayat
+            </a>
         </div>
         <div class="col-4 ">
-            <x-icon.home></x-icon.home>
-            <a href="" class="text-decoration-none text-dark d-block">Beranda</a>
+            <a href="" class="text-decoration-none text-dark ">
+                <x-icon.home></x-icon.home><br>Beranda
+            </a>
         </div>
         <div class="col-4 ">
-            <x-icon.user-profile></x-icon.user-profile>
-            <a href="" class="text-decoration-none text-dark d-block">Profil</a>
+            <a href="" class="text-decoration-none text-dark ">
+                <x-icon.user-profile></x-icon.user-profile><br>Profil
+            </a>
         </div>
     </div>
 </nav>
