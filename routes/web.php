529e7fdc8c6a3c58c105343b6575681aa69cd4dc
mysql73 <arfan2173@gmail.com>
2021-11-08 00:12:08 +0700

    edit user migration

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home')->render();
-});
+})->middleware("auth");
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
