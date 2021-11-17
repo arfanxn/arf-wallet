b8d3e0e6bf70b942887778d61eebdd4c6312208d
mysql73 <arfan2173@gmail.com>
2021-11-18 02:31:19 +0700

    removing migration discount_coupons

--- database/migrations/2021_11_17_031724_create_transactions_table.php
+++ database/migrations/2021_11_17_031724_create_transactions_table.php
@@ -18,8 +18,8 @@ class CreateTransactionsTable extends Migration
             // $table->foreignId("from_user_id")->constrained("users", "id");
             $table->foreignId("from_wallet_id")->constrained("wallets", "id");
             // $table->foreignId("to_user_id")->constrained("users", "id");
-            $table->foreignId("to_wallet_id")->constrained("wallets", "id");
-            $table->unsignedBigInteger("discount_id")->nullable();
+            $table->foreignId("to_wallet_id")->nullable()->constrained("wallets", "id");
+            $table->foreignId("transaction_type_id")->constrained("transaction_types", "id");
             $table->unsignedBigInteger("amount");
             $table->unsignedBigInteger("charge")->nullable();
             $table->string("title");
