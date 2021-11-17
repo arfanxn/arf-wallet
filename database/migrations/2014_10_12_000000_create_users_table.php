a54531dd6c91ac8cf071175ac3dd7e0ec9be1fec
mysql73 <arfan2173@gmail.com>
2021-11-17 07:24:35 +0700

    add model wallet.php,transaction.php and modify users_migration,password_reset_migration and add user_details_migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->string("phone_number")->unique();
-            $table->binary("active")->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
