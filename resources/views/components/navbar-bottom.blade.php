54227d87e58533cf3ac845a2fc8af9d03d030c01
mysql73 <arfan2173@gmail.com>
2021-11-22 07:19:55 +0700

    add some links in navbar bottom

--- resources/views/components/navbar-bottom.blade.php
+++ resources/views/components/navbar-bottom.blade.php
@@ -1,17 +1,17 @@
 <nav class="fixed-bottom bg-light offset-md-4 col-12 col-md-4 border-top border-dark pt-1  ">
     <div class="row text-center">
         <div class="col-4 ">
-            <a href="" class="text-decoration-none text-dark ">
+            <a href="{{ route('transaction-history') }}" class="text-decoration-none text-dark ">
                 <x-icon.history></x-icon.history><br>Riwayat
             </a>
         </div>
         <div class="col-4 ">
-            <a href="" class="text-decoration-none text-dark ">
+            <a href="{{ route('home') }}" class="text-decoration-none text-dark ">
                 <x-icon.home></x-icon.home><br>Beranda
             </a>
         </div>
         <div class="col-4 ">
-            <a href="" class="text-decoration-none text-dark ">
+            <a href="{{ route('account.index') }}" class="text-decoration-none text-dark ">
                 <x-icon.user-profile></x-icon.user-profile><br>Profil
             </a>
         </div>
