d2cf857267b4daf062dd882c52e1d49f0393578b
mysql73 <arfan2173@gmail.com>
2021-11-25 21:25:12 +0700

    Create new route file , and new tyransactionHistroycontroller

--- app/Http/Controllers/FrontendAPI/TransactionHistoriesController.php
+++ app/Http/Controllers/FrontendAPI/TransactionHistoriesController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 
-class TransactionHistoryController extends Controller
+class TransactionHistoriesController extends Controller
 {
     /**
      * Display a listing of the resource.
