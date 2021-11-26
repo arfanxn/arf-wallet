16ca774a163049e3781490ac9fbba5fd5a145d2e
mysql73 <arfan2173@gmail.com>
2021-11-27 00:04:06 +0700

    modify component class with merge

--- resources/views/components/icon/wallet.blade.php
+++ resources/views/components/icon/wallet.blade.php
@@ -1,3 +1,11 @@
-<div class="p-1 d-inline">
+@props([
+    'withBrand' => false,
+    'brandClass' => '',
+])
+
+<div {{ $attributes->merge(['class' => 'p-1 d-inline']) }}>
     <img class="" src="{{ asset('icon/wallet.png') }}" alt="">
+    @if ($withBrand)
+        <h5 class="ms-1 fw-bold d-inline align-middle {{ $brandClass }}">{{ config('app.name') }}</h5>
+    @endif
 </div>
--- resources/views/components/transaction-header.blade.php
+++ resources/views/components/transaction-header.blade.php
@@ -1,6 +1,7 @@
 @props(['route' => '/'])
 
-<header class="bg-primary fixed-top  offset-md-4 col-12 col-md-4 text-white py-1 px-2">
+<header
+    {{ $attributes->merge(['class' => 'bg-primary fixed-top  offset-md-4 col-12 col-md-4 text-white  py-1 px-2']) }}>
     <h5 class="mt-1">
         <a href="{{ $route }}" class="me-2 text-decoration-none text-white ">&#10094;</a>
         {{ $slot }}
