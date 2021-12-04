69b8397e37ffaf67443bc7616f5c421b38c28172
mysql73 <arfan2173@gmail.com>
2021-12-05 00:06:36 +0700

    adding new view composer array

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -28,7 +28,8 @@ class ViewServiceProvider extends ServiceProvider
         //  VIEW COMPOSER AUTH WALLET 
         View::composer([
             "components.navbar-top", "home", "transactions.histories",
-            "transactions.detail", "accounts.index", "components.modal.wallet-info"
+            "transactions.detail", "accounts.index", "components.modal.wallet-info",
+            "transactions.send-money-to"
         ], function ($view) {
             $view->with("authWallet", Auth::user()->wallet);
         });
