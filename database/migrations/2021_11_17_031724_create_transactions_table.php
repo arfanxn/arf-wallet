bcab028ec24b5142b0a87a07fcd636a1f83826eb
mysql73 <arfan2173@gmail.com>
2021-12-06 01:50:30 +0700

    edit string lengt value

--- database/migrations/2021_11_17_031724_create_transactions_table.php
+++ database/migrations/2021_11_17_031724_create_transactions_table.php
@@ -25,7 +25,7 @@ class CreateTransactionsTable extends Migration
             $table->foreignId("to_wallet_id")->constrained("wallets", "id");
             $table->unsignedBigInteger("amount");
             $table->unsignedInteger("charge")->default(0);
-            $table->string("description", 1000)->nullable();
+            $table->string("description", 255)->nullable();
             $table->boolean("status");
             $table->timestamps();
         });
