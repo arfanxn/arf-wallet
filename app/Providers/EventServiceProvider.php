2c4ee8f37a288f78965b02625579a778ba346e7d
mysql73 <arfan2173@gmail.com>
2021-12-06 01:31:16 +0700

    add observe in boot method

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Transaction;
 use App\Models\User;
+use App\Observers\TransactionObserver;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -30,6 +32,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         User::observe(UserObserver::class);
+        Transaction::observe(TransactionObserver::class);
     }
 
     public function shouldDiscoverEvents()
