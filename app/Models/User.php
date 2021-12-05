a1ade641bf2aeff7c3e453510e5e5b320348066c
mysql73 <arfan2173@gmail.com>
2021-12-06 01:28:39 +0700

    deleting unnecessary namepsace

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Wallet;
-use UserTransactionTrait;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
