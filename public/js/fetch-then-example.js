58d95cdd15e6863b2ff1baa051ca00c55745b8e6
mysql73 <arfan2173@gmail.com>
2021-12-07 20:52:27 +0700

    rename sorting to fetchthenexample

--- public/js/fetch-then-example.js
+++ public/js/fetch-then-example.js
@@ -1,38 +1,3 @@
-let transactionListsWrapper = document.querySelector("#transactionListsWrapper");
-let transactions = document.querySelectorAll(".transaction");
-
-
-function sortByOldest() {
-    transactionListsWrapper.innerHTML = '';
-    let reversed = [];
-    for (let i = transactions.length; i > 0; i--) {
-        // reversed.push(transactions[i - 1])
-        let transactionData = JSON.parse(transactions[i - 1].getAttribute("data-transaction"));
-        let authWallet = JSON.parse(transactions[i - 1].getAttribute("data-authWallet"))
-
-        transactionListsWrapper.innerHTML += `<a href="/transaction/detail/${transactionData.tx_hash}" data-transaction="${transactionData}"
-                class="transaction d-flex justify-content-between py-3 border-bottom border-secondary mx-3 text-decoration-none text-dark">
-                <div class="my-auto">
-                    <span class="d-block">
-                        ${authWallet.id == transactionData.from_wallet_id ? 'Kirim Uang' : 'Terima Uang' }</span>
-                    <small>${transactionData.created_at}</small>
-                </div>
-                <div class="my-auto">
-                    <span class="align-middle">${toIDR(transactionData.amount)}</span>
-                </div>
-            </a>`
-    }
-}
-
-
-
-
-
-
-
-
-
-
 // let radioBtnSorting = document.getElementsByName("transactions-sorting");
 // radioBtnSorting.forEach((elem) => {
 //     let isChecked = elem.checked;
