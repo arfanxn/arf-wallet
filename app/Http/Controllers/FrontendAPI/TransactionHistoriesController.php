98a49ff974508e93b3a175e666dc6be6c009580e
mysql73 <arfan2173@gmail.com>
2021-12-09 03:10:50 +0700

    create filterByDateAndTransactionType method

--- app/Http/Controllers/FrontendAPI/TransactionHistoriesController.php
+++ app/Http/Controllers/FrontendAPI/TransactionHistoriesController.php
@@ -11,14 +11,63 @@ use Illuminate\Support\Facades\Auth;
 
 class TransactionHistoriesController extends Controller
 {
-    public function newestToday()
+    // 
+    public function filterByDateAndTransactionType(Request $request)
     {
-        // 
+        // arf-wallet.test/fe-api/controller?transaction-type=send-money&filter-date=today
+        // dd($request->get("filter-date"), $request->get("transaction-type"));
+
+        $transactions = Auth::user()->wallet;
+        $transactionType =  $request->get("transaction-type") ?? "all";
+        $filterDate = $request->get("filter-date") ?? "all";
+
+        switch ($transactionType) {
+            case "send-money":
+                $transactions = $transactions->transferedTransactions();
+                break;
+            case "receive-money":
+                $transactions = $transactions->receivedTransactions();
+                break;
+            default: // show all
+                $transactions = $transactions->allTransactions();
+                break;
+        }
+
+        switch ($filterDate) {
+            case "today":
+                $transactions = $transactions->where("created_at", ">=", now()->startOfDay()->toDateTimeString());
+                break;
+            case "this-week":
+                $transactions = $transactions
+                    ->where("created_at", ">=", now()->startOfWeek()->toDateTimeString());
+                break;
+            case "this-month":
+                $transactions = $transactions
+                    ->where("created_at", ">=", now()->startOfMonth()->toDateTimeString());
+                break;
+            case "range-3-month":
+                $transactions = $transactions
+                    ->where("created_at", ">=", now()->subMonth(03)->toDateTimeString());
+                break;
+            case "this-year":
+                $transactions = $transactions
+                    ->where("created_at", ">=", now()->startOfYear()->toDateTimeString());
+                break;
+            default: // show all
+                $transactions = $transactions;
+                break;
+        }
+
+        $transactions =  $transactions->simplePaginate(15);
+
+        $transactions->withPath(url()->current() .
+            "?transaction-type=" .  $transactionType .
+            "&filter-date=" . $filterDate);
+
+        return TransactionResource::collection($transactions);
     }
 
-    public function oldestToday()
-    {
-    }
+    // public function getByRangeOneMonthAndTypeSendMoney
 
     /**
      * Display a listing of the resource.
