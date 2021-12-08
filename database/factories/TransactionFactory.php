fadda9fca86904b5c370cadb99e2571a4900ccfe
mysql73 <arfan2173@gmail.com>
2021-12-09 03:11:19 +0700

    add random factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -15,21 +15,32 @@ class TransactionFactory extends Factory
     public function definition()
     {
         // transfer
-        return [
-            "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
-            "from_wallet_id" => 1,
-            "to_wallet_id" => rand(2, 4),
-            "amount" => rand(10000, 999999),
-            "charge" => rand(0, 7000),
-            "description" => $this->faker->text(100),
-            "status" => 1,
-        ];
+        // return [
+        //     "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
+        //     "from_wallet_id" => 1,
+        //     "to_wallet_id" => rand(2, 9),
+        //     "amount" => rand(10000, 999999),
+        //     "charge" => rand(0, 7000),
+        //     "description" => $this->faker->text(100),
+        //     "status" => 1,
+        // ];
 
         // received 
+        // return [
+        //     "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
+        //     "from_wallet_id" => rand(2, 5),
+        //     "to_wallet_id" => 1,
+        //     "amount" => rand(10000, 999999),
+        //     "charge" => rand(0, 7000),
+        //     "description" => $this->faker->text(100),
+        //     "status" => 1,
+        // ];
+
+        // random
         return [
             "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
-            "from_wallet_id" => rand(2, 6),
-            "to_wallet_id" => 1,
+            "from_wallet_id" => rand(1, 5),
+            "to_wallet_id" => rand(6, 9),
             "amount" => rand(10000, 999999),
             "charge" => rand(0, 7000),
             "description" => $this->faker->text(100),
