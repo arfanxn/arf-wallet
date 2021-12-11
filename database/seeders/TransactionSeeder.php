779fc31d86ce72736d85eab6cfddb008f37eda92
mysql73 <arfan2173@gmail.com>
2021-12-12 06:02:22 +0700

    make TransactionSeeder

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TransactionSeeder extends Seeder
 {
@@ -14,21 +15,24 @@ class TransactionSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 200; $i++) {
-            Transaction::create([
-                "from_wallet_id" => rand(11, 20),
-                "to_wallet_id" => rand(1, 10),
-                "amount" => rand(10000, 9999999999),
-                "charge" => rand(1000, 10000),
-                "description" => "Receive Money -> Hello im iteration number " . $i,
-            ]);
-            Transaction::create([
-                "from_wallet_id" => rand(0, 10),
-                "to_wallet_id" => rand(11, 20),
-                "amount" => rand(10000, 9999999999),
-                "charge" => rand(1000, 10000),
-                "description" => "Send Money -> Hello im iteration number " . $i,
-            ]);
-        }
+        Transaction::create([
+            "tx_hash" =>
+            strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
+            "from_wallet_id" => rand(11, 20),
+            "to_wallet_id" => rand(1, 10),
+            "amount" => rand(10000, 9999999999),
+            "charge" => rand(1000, 10000),
+            "description" => "Receive Money Hello im iteration number ",
+        ]);
+
+        Transaction::create([
+            "tx_hash" =>
+            strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
+            "from_wallet_id" => rand(0, 10),
+            "to_wallet_id" => rand(11, 20),
+            "amount" => rand(10000, 9999999999),
+            "charge" => rand(1000, 10000),
+            "description" => "Send Money Hello im iteration number ",
+        ]);
     }
 }
