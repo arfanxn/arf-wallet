31fd3f3867acf3e7906281c45dd98bae45a4fa19
mysql73 <arfan2173@gmail.com>
2021-12-11 09:24:58 +0700

    delete unecesary route

--- routes/frontend-api.php
+++ routes/frontend-api.php
@@ -6,22 +6,11 @@ use App\Http\Controllers\FrontendAPI\TransactionHistoriesController;
 use App\Http\Resources\TransactionResource;
 use App\Http\Resources\WalletResource;
 use App\Models\Transaction;
+use App\Models\Wallet;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get("test", function () {
-    // return (new WalletResource(Auth::user()->wallet
-    //     ->loadMissing("owner",  "transferedTransactions.toWallet.owner", "receivedTransactions.fromWallet.owner")));
-
-    return;
-
-    return now()->subMonth(03)->format("M");
-
-    return Transaction::where("from_wallet_id", 1)
-        ->orWhere("to_wallet_id", 1)->simplePaginate(12);
 });
 
 Route::post("transaction/history/filter", [TransactionHistoriesController::class, "filterByDateAndTransactionType"]);
-
-// Route::post("transactions/sorting/oldest", [TransactionHistoriesController::class, "oldest"]);
-// Route::post("transactions/sorting/newest", [TransactionHistoriesController::class, "newest"]);
