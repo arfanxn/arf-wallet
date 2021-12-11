27273deeaba662c54d636dcac170d695a7c7b4de
mysql73 <arfan2173@gmail.com>
2021-12-11 23:57:28 +0700

    make component for alerting login/register input error

---
+++ resources/views/components/auth/input-error-alert.blade.php
@@ -0,0 +1,10 @@
+@props(['error' => 'email'])
+
+@error($error)
+    <div class="px-5 d-block">
+        <div class="alert alert-danger rounded  alert-dismissible fade show py-0 mt-1" role="alert">
+            <strong>{{ $message }}</strong>
+            <button type="button" class="btn-close  p-1" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@enderror
