af605540b977435603657dec674c8cde37ddaf3a
mysql73 <arfan2173@gmail.com>
2021-12-15 06:21:33 +0700

    make dinamy class

--- resources/views/components/modal/_modal-background.blade.php
+++ resources/views/components/modal/_modal-background.blade.php
@@ -1,7 +1,7 @@
-@props(['id' => ''])
+@props(['id' => '', 'bgColor' => 'rgba(0,0,0,.5)'])
 
 <div {{ $id ? 'id="' . $id . '"' : '' }}
-    class="position-absolute top-0 start-50 translate-middle-x w-100 h-100 d-flex offset-md-4 offset-0"
-    style="background-color: rgba(0,0,0,.5) ;  z-index: 99999;">
+    {{ $attributes->merge(['class' => 'position-absolute top-0 start-50 translate-middle-x w-100 h-100 d-flex offset-md-4 offset-0']) }}
+    style="background-color: {{ $bgColor }} ;  z-index: 99999;">
     {{ $slot }}
 </div>
