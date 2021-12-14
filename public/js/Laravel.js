53c649e12598c80cc025e6168fed87a2e02197f2
mysql73 <arfan2173@gmail.com>
2021-12-15 06:23:56 +0700

    make new laravel method helper for form validation

--- public/js/Laravel.js
+++ public/js/Laravel.js
@@ -3,6 +3,39 @@ class Laravel {
         return document.querySelector(`meta[name="csrf-token"]`).content;
     }
 
+    static async formValidator(inputObject, URLToRequestValidate, fetchOptionOrCallback = null, callback = null) {
+        try {
+            let fetchOption;
+            if (typeof fetchOptionOrCallback == "object") {
+                fetchOption = fetchOptionOrCallback;
+            } else fetchOption = null
+
+            let response = await fetch(URLToRequestValidate, !fetchOption ? {
+                method: "post",
+                credentials: "same-origin",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Accept": 'application/json',
+                    "X-CSRF-Token": Laravel.CSRF()
+                },
+                body: JSON.stringify(inputObject)
+            } : fetchOption);
+
+            if (!response.ok) throw new Error(`HTTP error -> status: ${response.status}`);
+
+            if (typeof fetchOptionOrCallback == "function" || typeof callback == "function") {
+                callback = callback ? callback : fetchOptionOrCallback;
+                return await callback(response);
+            }
+
+            let jsonObj = await response.json();
+
+            return jsonObj;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     static toDateTimeString(dateTime) {
         return dateTime.replace(/[T]/, " ").replace(/[.].*/g, "");
     }
