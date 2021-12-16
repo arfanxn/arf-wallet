12359cc2483ea8e148f5d119ee09014de9c65216
mysql73 <arfan2173@gmail.com>
2021-12-17 00:02:25 +0700

    fixing if else condition

--- app/Repositories/WalletRepository.php
+++ app/Repositories/WalletRepository.php
@@ -11,9 +11,11 @@ class WalletRepository
 {
     public static function getRecentTransferedWalletsByWallet(Wallet|int $walletOrID, int $limit = 10)
     {
-        if ($walletOrID instanceof Wallet) $wallet = $walletOrID->id;
+        if ($walletOrID instanceof Wallet) $walletOrID = $walletOrID->id;
+        $walletID = $walletOrID;
+
         $transferedTransactions = Transaction::with("toWallet.owner")
-            ->where("from_wallet_id", $wallet)
+            ->where("from_wallet_id", $walletID)
             ->orderBy("created_at", "desc")
             ->limit($limit)->get()->unique("to_wallet_id");
 
@@ -27,20 +29,21 @@ class WalletRepository
         return $recentWallets;
     }
 
-    public static function getWalletByUserIDorCreateIfNotExist(User|string|int $user_or_id)
+    public static function getByUserIDorCreateIfNotExist(User|string|int $user_or_id)
     {
-        $user_id = ($user_or_id instanceof Wallet) ? $user_or_id = $user_or_id->id :
-            $user_or_id;
+        if ($user_or_id instanceof User) $user_or_id  =  $user_or_id->id;
+        $user_id = $user_or_id;
+
         return  Wallet::where("user_id", $user_id)->first() ?? static::createByUserID($user_id);
     }
 
     public static function createByUserID(User|string|int $user_or_id)
     {
-        $user_id = ($user_or_id instanceof Wallet) ? $user_or_id = $user_or_id->id :
-            $user_or_id;
+        if ($user_or_id instanceof User) $user_or_id = $user_or_id->id;
+        $user_id = $user_or_id;
 
         return Wallet::create([
-            "user_id" => $user_or_id,
+            "user_id" => strval($user_id),
             "address" => strtoupper(Str::random(16)),
             "amount" => 0,
         ]);
@@ -48,8 +51,9 @@ class WalletRepository
 
     public static function createByUserIDandGetCreated(User|string|int $user_or_id)
     {
-        $user_id = ($user_or_id instanceof Wallet) ? $user_or_id = $user_or_id->id :
-            $user_or_id;
+        if ($user_or_id instanceof User) $user_or_id  =  $user_or_id->id;
+        $user_id = $user_or_id;
+
         static::createByUserID($user_id);
         return Wallet::where("user_id", $user_id)->first();
     }
