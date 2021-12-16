7bc4e032f05c8b946e85466d39090ab8b4c1d484
mysql73 <arfan2173@gmail.com>
2021-12-17 00:09:01 +0700

    deleting unecesary namespace ,  and make if else condition if user fail or server fail

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use App\Responses\ErrorMessageResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -54,20 +54,33 @@ class RegisterController extends Controller
         ]);
 
         User::create([
-            "name" => $request->session()->pull("fullname"),
+            "name" => $request->session()->get("fullname"),
             "email" => $request->session()->get("email"),
-            "phone_number" => $request->session()->pull("phone_number"),
+            "phone_number" => $request->session()->get("phone_number"),
             "password" => bcrypt($request->pin_number)
         ]);
 
         $isLoginSuccess =  Auth::attempt([
-            "email" => $request->session()->pull("email"),
+            "email" => $request->session()->get("email"),
             "password" => $request->pin_number,
         ], remember: 1);
 
-        return $isLoginSuccess ? redirect()->to(RouteServiceProvider::HOME())
-            ->with(["success" =>
-            "Akun anda telah teregistrasi, kini anda dapat login dengan Email."])
-            :  redirect()->route("register.create");
+        $isLoginSuccess = false;
+
+        if ($isLoginSuccess) {
+            $request->session()->flush();
+            return redirect()->to(RouteServiceProvider::HOME())
+                ->with(["success" =>
+                "Akun anda telah teregistrasi, kini anda dapat login dengan Email."]);
+        } else {
+            return redirect()->route("register.create")
+                ->withErrors(["server_error" => ErrorMessageResponse::serverError()])
+                ->withInput([
+                    "fullname" => $request->session()->get("fullname"),
+                    "email" => $request->session()->get("email"),
+                    "phone_number" => $request->session()->get("phone_number"),
+                    "pin_number" => $request->pin_number,
+                ]);
+        }
     }
 }
