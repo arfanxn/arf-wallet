ddb477c8ef3f8219704adbc5eed8fee8178c28ff
mysql73 <arfan2173@gmail.com>
2021-12-19 02:28:12 +0700

    make new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Transaction;
+use App\Models\Wallet;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -17,16 +18,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::create([
-            "name" => "muhammad arfan",
+            "name" => "Muhammad Arfan",
             "phone_number" => "089506089254",
             "email" => "arfan@gm.com",
             "password" => bcrypt("111222"),
         ]);
+        Wallet::where("user_id", "<=", 5)->update(["balance" => "18446744073709551615"]);
 
-        \App\Models\User::factory(20)->create();
+        \App\Models\User::factory(200)->create();
 
-        for ($i = 0; $i < 1000; $i++) {
-            \App\Models\Transaction::factory(10000)->create();
-        }
+        \App\Models\Transaction::factory(1000)->create();
+
+        // for ($i = 0; $i < 10000; $i++) {
+        //     try {
+        //         \App\Models\User::factory(100)->create();
+        //     } catch (\Illuminate\Database\QueryException $e) {
+        //         continue;
+        //     }
+        // }
+        // // for ($i = 0; $i < 100; $i++) {
+        // //     \App\Models\User::factory(100)->create();
+        // // }
+
+        // // for ($i = 0; $i < 1000; $i++) {
+        // //     \App\Models\Transaction::factory(10000)->create();
+        // // }
     }
 }
