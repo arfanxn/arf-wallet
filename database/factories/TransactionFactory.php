ba721edea44b10b6847da27812684c3445ebd708
mysql73 <arfan2173@gmail.com>
2021-12-18 07:04:50 +0700

    deleting comments in TransactionFactory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -14,29 +14,10 @@ class TransactionFactory extends Factory
      */
     public function definition()
     {
-        // transfer
-        // return [
-        //     "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
-        //     "from_wallet_id" => 1,
-        //     "to_wallet_id" => rand(2, 9),
-        //     "amount" => rand(10000, 999999),
-        //     "charge" => rand(0, 7000),
-        //     "description" => $this->faker->text(100),
-        // ];
-
-        // received 
-        // return [
-        //     "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
-        //     "from_wallet_id" => rand(2, 5),
-        //     "to_wallet_id" => 1,
-        //     "amount" => rand(10000, 999999),
-        //     "charge" => rand(0, 7000),
-        //     "description" => $this->faker->text(100),
-        // ];
 
         // random
-        $from = rand(1, 10);
-        $to = rand(1, 10);
+        $from = rand(1, 20);
+        $to = rand(1, 20);
         return [
             "tx_hash" => strtoupper(Str::random(10)) . preg_replace("/[^0-9]+/",  "", now()->toDateTimeString()),
             "from_wallet_id" =>  $from != $to ? $from : $from + 1,
