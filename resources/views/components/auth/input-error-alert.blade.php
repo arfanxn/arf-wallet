7862503e9d36d8f597ccfbebd5f61e40d1b35258
mysql73 <arfan2173@gmail.com>
2021-12-20 01:31:20 +0700

    fixing class atibutes bug

--- resources/views/components/auth/input-error-alert.blade.php
+++ resources/views/components/auth/input-error-alert.blade.php
@@ -1,7 +1,7 @@
 @props(['error' => 'email'])
 
 @error($error)
-    <div $attributes->merge(["class" => "px-5 d-block"])>
+    <div {{ $attributes->merge(['class' => 'px-5 d-block']) }}>
         <div class="alert alert-danger rounded  alert-dismissible fade show py-0 mt-1" role="alert">
             <strong>{{ $message }}</strong>
             <button type="button" class="btn-close  p-1" data-bs-dismiss="alert" aria-label="Close"></button>
