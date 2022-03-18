de88e4dcaa8f3e53cf78e98a7ce961b3f2b9a1e1
mysql73 <arfan2173@gmail.com>
2022-03-18 10:58:36 +0700

    fixed image not displayed

--- resources/views/components/profile-picture.blade.php
+++ resources/views/components/profile-picture.blade.php
@@ -1,8 +1,8 @@
-<?php $defaultProfilePicture = asset('accounts/profile_pictures/default.jpg'); ?>
+<?php $defaultProfilePicture = asset('storage/accounts/profile_pictures/default.jpg'); ?>
 
 {{-- @dump($defaultProfilePicture) --}}
 
 @props(['src' => false, 'width' => 55, 'height' => 55])
 <img {{ $attributes->merge(['class' => ' rounded-circle border border-white border-2']) }}
-    src="{{ $src ? asset('accounts/profile_pictures/' . $src) : $defaultProfilePicture }}" width="{{ $width }}"
-    height="{{ $height }}">
+    src="{{ $src ? asset('storage/accounts/profile_pictures/' . $src) : $defaultProfilePicture }}"
+    width="{{ $width }}" height="{{ $height }}">
